50efe2291df686cda155003e0ba79ce728f61e70
Evan Finkelstein <evanfinkelstein91@gmail.com>
2020-11-02 18:24:00 -0800

    s

--- data/animals.js
+++ data/animals.js
@@ -1,14 +1,17 @@
 module.exports = [
   {
     name: 'bessie',
-    cool_factor: 3,
+    cool_factor: 377,
+    type: 'meat'
   },
   {
     name: 'jumpy',
     cool_factor: 4,
+    type: 'meat'
   },
   {
     name: 'spot',
     cool_factor: 10,
+    type: 'meat'
   }
 ];
